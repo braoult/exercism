2342d9b7f6b80aa512c02f91e63abe23108d8503
Bruno Raoult <braoult@gmail.com>
2021-08-11 15:52:27 +0200

    templates: move includes from ex.c to ex.h

--- c/templates/ex.c
+++ c/templates/ex.c
@@ -1,13 +1,6 @@
 /* See GNUmakefile below for explanation
  * https://github.com/braoult/exercism/blob/master/c/templates/GNUmakefile
  */
-#if defined UNIT_TEST || defined DEBUG
-#include <stdio.h>
-#include <stdlib.h>
-#endif
-
-
-
 #ifdef UNIT_TEST
 int main(int ac, char **av)
 {
--- c/templates/ex.h
+++ c/templates/ex.h
@@ -1,6 +1,10 @@
 /* See GNUmakefile below for explanation
  * https://github.com/braoult/exercism/blob/master/c/templates/GNUmakefile
  */
+#if defined UNIT_TEST || defined DEBUG
+#include <stdio.h>
+#include <stdlib.h>
+#endif
 #ifdef  TESTALL
 #undef  TEST_IGNORE
 #define TEST_IGNORE() {}
